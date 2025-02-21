98fe28b250bd30e49407a3f2a8d350b56865a0b5
Pham Trung Ky <phamtrungky@gmail.com>
2025-02-21 18:34:07 +0700

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.loginregister"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.loginregister"
